0c824c7dcbae8b849e981f350e8c63699da48cd7
Beno <103076866+benovojnikovic@users.noreply.github.com>
2022-06-09 16:36:10 +0000

    adds pages

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-echo'<h1>Calculator!</h1>;
+echo '<h1>Calculator!</h1>';
 
-  include 'Calculator.php';
+include 'Calculator.php';
 
-  $myCalc = new Calculator();
+$myCalc = new Calculator();
 
-  echo $myCalc->add(2,2);
+echo $myCalc->add(2,2);
